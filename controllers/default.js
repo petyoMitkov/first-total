8ab7c42888a2f22f8a107429a2c57b06c882e54c
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-09-25 16:16:06 +0300

    Delete

--- controllers/default.js
+++
@@ -1,9 +0,0 @@
-exports.install = function() {
-	F.route('/');
-	F.route('/services/');
-	F.route('/contact/');
-	F.route('/references/');
-
-	// Enables a localization mechanism + compression for all client-side components
-	F.localize('/components/*.html', ['compress']);
-};
--- public/js/default.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$(document).on('click', '.mainmenu-button', function() {
-		$(this).parent().find('nav').toggleClass('mainmenu-visible');
-	});
-});
